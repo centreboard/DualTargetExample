284463c0dee0ef058947d4d0d8943ffd68b2ddc7
Matthew Johnson <MAJOHNSON@bw-npe.net>
2020-10-22 15:55:17 +0100

    Fails Value Destination

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public string Method1(string param1) {
+            return $"Hello {param1}";
+        }
+    }
+}
